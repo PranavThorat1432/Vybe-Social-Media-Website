91ed3d031672968492f7af5725c40c6263dc639d
PranavThorat1432 <pranavthorat95@gmail.com>
2025-11-20 19:24:26 +0530

    post Routes

---
+++ Server/routes/postRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import isAuth from '../middlewares/isAuth.js';
+import { upload } from '../middlewares/multer.js';
+import { comment, getAllPosts, like, saved, uploadPost } from "../controllers/postControllers.js";
+
+const postRouter = express.Router();
+
+postRouter.post('/upload-post', isAuth, upload.single('media'), uploadPost);
+postRouter.get('/getAll-posts', isAuth, getAllPosts);
+postRouter.get('/like/:postId', isAuth, like);
+postRouter.post('/comment/:postId', isAuth, comment);
+postRouter.get('/saved/:postId', isAuth, saved);
+
+export default postRouter;
