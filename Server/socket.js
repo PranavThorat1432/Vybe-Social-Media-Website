c35d5279b2ddf9043cd4afe48277a7465db147fc
PranavThorat1432 <pranavthorat95@gmail.com>
2025-11-25 13:20:52 +0530

    Socket File

---
+++ Server/socket.js
@@ -0,0 +1,36 @@
+import http from 'http';
+import express from 'express';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+        methods: ['GET', 'POST']
+    }
+});
+
+const userSocketMap = {};
+
+export const getSocketId = (receiverid) => {
+    return userSocketMap[receiverid];
+};
+
+io.on('connection', (socket) => {
+    const userId = socket.handshake.query.userId;
+    if(userId != undefined && userId != 'undefined') {
+        userSocketMap[userId] = socket.id
+        io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    }
+    
+    socket.on('disconnect', () => {
+        if(userId != undefined && userId != 'undefined') {
+            delete userSocketMap[userId]
+            io.emit('getOnlineUsers', Object.keys(userSocketMap));
+        }
+    });
+});
+
+export {app, io, server}; 
