c932a24b80f81da120aff551c5129af4cd8a25c6
PranavThorat1432 <pranavthorat95@gmail.com>
2025-11-29 23:36:07 +0530

    Story aPage

---
+++ Client/src/Pages/Story.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { serverUrl } from '../App';
+import { useDispatch, useSelector } from 'react-redux';
+import { setStoryData } from '../redux/storySlice';
+import StoryCard from '../Components/StoryCard';
+
+const Story = () => {
+
+    const {userName} = useParams();
+    const dispatch = useDispatch();
+    const {storyData} = useSelector((state) => state.story);
+
+    const handleStory = async () => {
+        dispatch(setStoryData(null));
+        try {
+            const result = await axios.get(`${serverUrl}/api/story/getStory/${userName}`, {
+                withCredentials: true
+            });
+            dispatch(setStoryData(result.data[0]));
+            console.log(storyData)
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        if(userName) {
+            handleStory();
+        }
+    }, [userName]);
+
+    
+  return (
+    <div className='w-full h-screen bg-black flex justify-center items-center'>
+        <StoryCard story={storyData}/>
+    </div>
+  )
+}
+
+export default Story
